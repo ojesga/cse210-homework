11718575474d15b736fb5802a6e7fcb097016a1f
ojesga <seyibamgboye299@gmail.com>
2026-05-09 19:35:58 -0700

    Added a comment

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
+{    static void Main(string[] args)
+   
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask the user for their first name and last name, then display them in the format: "Your name is: last, first last"
+        Console.Write("what is your first name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+        Console.Write($"Your name is {last}, {first} {last}");
+        string fullName = $"{last}, {first} {last}";
     }
 }
