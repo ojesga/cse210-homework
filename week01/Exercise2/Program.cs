1089ae2d5d854b32a7f3fa79d857584b497c57c3
ojesga <seyibamgboye299@gmail.com>
2026-05-09 20:31:41 -0700

    added comment to the end of the line

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? "); // Use .Write so the user types on the same line
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time! You did not pass the class.");
+        }
     }
-}
+} 
